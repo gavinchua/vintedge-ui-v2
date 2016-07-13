e32e4e325bd7a8893ce7de92ca534dac6f19774c
Gavin Chua <me@gavinchua.com>
2016-07-13 15:20:52 +0800

    Update

--- tasks/options/usebanner.js
+++ tasks/options/usebanner.js
@@ -2,6 +2,7 @@ module.exports = {
 	css: {
 		options: {
 			banner: '/*!\n' +
+				' * CSS\n' +
 				' * <%= pkg.name %> - v<%= pkg.version %>\n' +
 				' * Last updated: <%= grunt.template.today("dd-mm-yyyy") %>\n' +
 				' * by <%= pkg.author %> - <%= pkg.homepage %>\n' +
@@ -16,10 +17,11 @@ module.exports = {
 	js: {
 		options: {
 			banner: '/*!\n' +
+				' * JS\n' +
 				' * <%= pkg.name %> - v<%= pkg.version %>\n' +
 				' * Last updated: <%= grunt.template.today("dd-mm-yyyy") %>\n' +
 				' * by <%= pkg.author %> - <%= pkg.homepage %>\n' +
-				' * Compiled using Grunt - http://learnboost.github.io/stylus/\n' +
+				' * Compiled using Grunt' +
 				//' * Licensed under MIT (<%= _.pluck(pkg.licenses, "url").join(", ") %>)\n' +
 				' */\n\n'
 		},
